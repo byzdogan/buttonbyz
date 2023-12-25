a844a96ec822e9f90ccf762b55dd64ee40427cf1
byzdogan <111748467+byzdogan@users.noreply.github.com>
2023-12-25 06:38:11 +0300

    Update App.js

--- example/src/App.js
+++ example/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Button type='primary'>Primary Button</Button>
-      <Button type='default'>Default Button</Button>
+      <Button>Default Button</Button>
       <Button type='dashed'>Dashed Button</Button>
       <Button type='text'>Text Button</Button>
       <Button type='link'>Link Button</Button>
